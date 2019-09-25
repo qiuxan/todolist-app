e4ef2956294240d699f241de2bf08caf1d6c1460
qiuxan <qiuxan@gmail.com>
2019-09-25 12:36:25 +1000

    react set state and bind value

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,17 +1,35 @@
 import React, { Component, Fragment } from "react";
 
 class TodoList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: "",
+      list: []
+    };
+  }
   render() {
     return (
       <Fragment>
         <div>
-          <input type="text" /> <button>submit</button>{" "}
+          <input
+            type="text"
+            value={this.state.inputValue}
+            onChange={this.handleInputChange.bind(this)}
+          />{" "}
+          <button>submit</button>{" "}
         </div>
         <ul>
           <li>learing english</li>
         </ul>
       </Fragment>
     );
+  }
+  handleInputChange(e) {
+    this.setState({
+      inputValue: e.target.value
+    });
+    // console.log(e.target.value);
   }
 }
 
