d03ad309975895da54e18ccc74e5c062f94fe703
qiuxan <qiuxan@gmail.com>
2019-11-11 22:45:45 +1100

    proptype and defaultprops

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-class Todolist extends Component {
+class TodoItem extends Component {
   constructor(props) {
     super(props);
     this.handleClick = this.handleClick.bind(this);
   }
   render() {
-    return <div onClick={this.handleClick}>{this.props.content}</div>;
+    const { content, test } = this.props;
+    return (
+      <div onClick={this.handleClick}>
+        {test}-{content}
+      </div>
+    );
   }
   handleClick() {
-    this.props.deleteItem(this.props.index);
+    const { deleteItem, index } = this.props;
+    deleteItem(index);
   }
 }
 
-export default Todolist;
+TodoItem.propTypes = {
+  test: PropTypes.string.isRequired,
+  content: PropTypes.string,
+  deleteItem: PropTypes.func,
+  index: PropTypes.number
+};
+
+TodoItem.defaultProps = {
+  test: "hello world"
+};
+
+export default TodoItem;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from "react";
 import TodoItem from "./TodoItem";
-
+import "./App.css";
 class TodoList extends Component {
   constructor(props) {
     super(props);
@@ -8,6 +8,9 @@ class TodoList extends Component {
       inputValue: "",
       list: []
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleBtnClick = this.handleBtnClick.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
   }
   render() {
     return (
@@ -16,47 +19,49 @@ class TodoList extends Component {
           <input
             type="text"
             value={this.state.inputValue}
-            onChange={this.handleInputChange.bind(this)}
+            onChange={this.handleInputChange}
           />{" "}
-          <button onClick={this.handleBtnClick.bind(this)}>submit</button>{" "}
+          <button onClick={this.handleBtnClick}>submit</button>{" "}
         </div>
-        <ul>
-          {this.state.list.map((item, index) => {
-            return (
-              <div>
-                <TodoItem
-                  content={item}
-                  index={index}
-                  deleteItem={this.handleItemDelete.bind(this)}
-                />
-                {/*               <li onClick={this.handleItemDelete.bind(this, index)}>{item}</li>
-                 */}
-              </div>
-            );
-          })}
-        </ul>
+        <ul>{this.getTodoItem()}</ul>
       </Fragment>
     );
   }
-  handleInputChange(e) {
-    this.setState({
-      inputValue: e.target.value
+
+  getTodoItem() {
+    return this.state.list.map((item, index) => {
+      return (
+        <TodoItem
+          key={index}
+          content={item}
+          index={index}
+          deleteItem={this.handleItemDelete}
+        />
+      );
     });
-    // console.log(e.target.value);
+  }
+  handleInputChange(e) {
+    const value = e.target.value;
+
+    this.setState(() => ({
+      inputValue: value
+    }));
   }
 
   handleBtnClick() {
-    this.setState({
-      list: [...this.state.list, this.state.inputValue],
-      inputValue: ""
+    this.setState(prevState => {
+      return {
+        list: [...prevState.list, prevState.inputValue],
+        inputValue: ""
+      };
     });
   }
 
   handleItemDelete(index) {
-    const list = [...this.state.list];
-    list.splice(index, 1);
-    this.setState({
-      list: list
+    this.setState(prevState => {
+      const list = [...prevState.list];
+      list.splice(index, 1);
+      return { list };
     });
   }
 }
