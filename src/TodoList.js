e5137991458e4c6c496296933b0b648c535cda84
qiuxan <qiuxan@gmail.com>
2020-02-20 22:14:32 +1100

    react- redux

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,77 +1,40 @@
 import React, { Component } from "react";
-import "antd/dist/antd.css";
-import store from "./store";
-import TodoListUI from "./TodoListUI";
-import axios from "axios";
-
-import {
-  getInputChangeAction,
-  getAddItemAction,
-  getDeleteItemAction,
-  getInitList
-} from "./store/actionCreators";
+import { connect } from "react-redux";
 
 class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    // console.log(store.getState());
-    /* store.getState() help to get the state from the store and the store get it from the reducer */
-
-    this.state = store.getState();
-    this.handleInputChange = this.handleInputChange.bind(this);
-    this.handleStoreChange = this.handleStoreChange.bind(this);
-    this.handleBtnClick = this.handleBtnClick.bind(this);
-    this.handleItemDelete = this.handleItemDelete.bind(this);
-    store.subscribe(this.handleStoreChange);
-  }
-
   render() {
     return (
-      <TodoListUI
-        inputValue={this.state.inputValue}
-        handleInputChange={this.handleInputChange}
-        handleBtnClick={this.handleBtnClick}
-        list={this.state.list}
-        handleItemDelete={this.handleItemDelete}
-      />
+      <div>
+        <div>
+          <input
+            type="text"
+            value={this.props.inputValue}
+            onChange={this.props.changeInputValue}
+          />
+          <button>Submit</button>
+        </div>
+        <ul>
+          <li>Dell</li>
+        </ul>
+      </div>
     );
   }
-
-  componentDidMount() {
-    const action = getInitList();
-    store.dispatch(action);
-    // console.log(action);
-    // axios.get("/list.json").then(res => {
-    //   const data = res.data;
-    //   const action = initListAction(data);
-    //   store.dispatch(action);
-    // });
-    // console.log(action);
-    // axios.get("/list.json").then(res => {
-    //   const data = res.data;
-    //   const action = initListAction(data);
-    //   store.dispatch(action);
-    // });
-  }
-
-  handleStoreChange() {
-    this.setState(store.getState());
-  }
-
-  handleInputChange(e) {
-    const action = getInputChangeAction(e.target.value);
-    store.dispatch(action);
-  }
-
-  handleBtnClick() {
-    const action = getAddItemAction();
-    store.dispatch(action);
-  }
-
-  handleItemDelete(index) {
-    const action = getDeleteItemAction(index);
-    store.dispatch(action);
-  }
 }
+const mapStateToProps = state => {
+  return {
+    inputValue: state.inputValue
+  };
+};
 
-export default TodoList;
+const mapDispatchToPros = dispatch => {
+  return {
+    changeInputValue(e) {
+      const action = {
+        type: "change_input_value",
+        value: e.target.value
+      };
+      dispatch(action);
+    }
+  };
+};
+export default connect(mapStateToProps, mapDispatchToPros)(TodoList);
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,42 +1,13 @@
-import {
-  CHANGE_INPUT_VALUE,
-  ADD_TODO_ITEM,
-  DELETE_TODO_ITEM,
-  INIT_LIST_ACTION
-} from "./actionType";
-
-const defaultState = {
-  inputValue: "",
+const defaulState = {
+  inputValue: "his",
   list: []
 };
 
-export default (state = defaultState, action) => {
-  if (action.type === CHANGE_INPUT_VALUE) {
-    // console.log(action);
-    const copyState = JSON.parse(JSON.stringify(state));
-    copyState.inputValue = action.value;
-    return copyState;
+export default (state = defaulState, action) => {
+  if (action.type === "change_input_value") {
+    const newState = JSON.parse(JSON.stringify(state));
+    newState.inputValue = action.value;
+    return newState;
   }
-
-  if (action.type === INIT_LIST_ACTION) {
-    const copyState = JSON.parse(JSON.stringify(state));
-    copyState.list = action.data;
-    return copyState;
-  }
-
-  if (action.type === ADD_TODO_ITEM) {
-    console.log(action);
-    const copyState = JSON.parse(JSON.stringify(state));
-    copyState.list.push(state.inputValue);
-    copyState.inputValue = "";
-    return copyState;
-  }
-
-  if (action.type === DELETE_TODO_ITEM) {
-    const copyState = JSON.parse(JSON.stringify(state));
-    copyState.list.splice(action.index, 1);
-    return copyState;
-  }
-
   return state;
 };
